016455046d7beb4b0a9ad85edf415246352dfb02
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-05-12 10:54:02 +0200

    Initial Commit

---
+++ Tasky/TaskyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskyApp.swift
+//  Tasky
+//
+//  Created by Mohsin Ali Ayub on 12.05.24.
+//
+
+import SwiftUI
+
+@main
+struct TaskyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
