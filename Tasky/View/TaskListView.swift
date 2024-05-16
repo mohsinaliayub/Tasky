6cfa5172041357e7df5c8273fc159f30b0ebb11d
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-05-16 22:35:47 +0200

    structuring the app

--- Tasky/View/TaskListView.swift
+++ Tasky/View/TaskListView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  TaskListView.swift
 //  Tasky
 //
 //  Created by Mohsin Ali Ayub on 12.05.24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct TaskListView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    TaskListView()
 }
