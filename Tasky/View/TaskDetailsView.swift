f87895d45202bd4675a176a24a03d0f595769e0d
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-05-19 20:03:30 +0200

    create a skeleton view for new task

--- Tasky/View/TaskDetailsView.swift
+++ Tasky/View/TaskDetailsView.swift
@@ -8,10 +8,66 @@
 import SwiftUI
 
 struct TaskDetailsView: View {
-    var task: Tasky.Task?
+    @State private var title: String = ""
     
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading, spacing: 32) {
+            VStack {
+                TextField("Enter task", text: $title)
+                    .font(.title2)
+                    .padding(.bottom, 8)
+                Divider()
+            }
+            
+            LabelAndContentView(systemName: "calendar") {
+                HStack {
+                    Text("Today")
+                        .padding(8)
+                        .background {
+                            RoundedRectangle(cornerRadius: 25)
+                                .strokeBorder(style: .init(lineWidth: 1, dash: [3]))
+                        }
+                    Text("Tomorrow")
+                        .padding(8)
+                        .background {
+                            RoundedRectangle(cornerRadius: 25)
+                                .strokeBorder(style: .init(lineWidth: 1, dash: [3]))
+                        }
+                    Text("Custom")
+                        .padding(8)
+                        .background {
+                            RoundedRectangle(cornerRadius: 25)
+                                .strokeBorder(style: .init(lineWidth: 1, dash: [3]))
+                        }
+                }
+            }
+            
+            LabelAndContentView(systemName: "bell") {
+                
+            }
+            
+            LabelAndContentView(systemName: "exclamationmark.triangle") {
+                
+            }
+            
+            Spacer()
+            
+        }
+        .padding()
+    }
+}
+
+struct LabelAndContentView<Content: View>: View {
+    let systemName: String
+    @ViewBuilder var content: Content
+    
+    var body: some View {
+        HStack {
+            Image(systemName: systemName)
+                .imageScale(.large)
+                .padding(.trailing)
+            content
+        }
     }
 }
 
