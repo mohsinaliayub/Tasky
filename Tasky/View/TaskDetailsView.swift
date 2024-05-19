bb89f65a4bed8764cf6c40f7ac09c5793b7283d1
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-05-19 19:17:32 +0200

    create task details view

---
+++ Tasky/View/TaskDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  TaskDetailsView.swift
+//  Tasky
+//
+//  Created by Mohsin Ali Ayub on 19.05.24.
+//
+
+import SwiftUI
+
+struct TaskDetailsView: View {
+    var task: Tasky.Task?
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    TaskDetailsView()
+}
