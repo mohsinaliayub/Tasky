e213b45928b7e213ec5047f62a0f21dfee23deac
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-05-28 12:29:23 +0200

    collapse/expand content   - use a transition to collapse/expand   - refactor code to be more readable

--- Tasky/View/Components/CollapsibleContainer/CollapsibleContent.swift
+++ Tasky/View/Components/CollapsibleContainer/CollapsibleContent.swift
@@ -12,6 +12,8 @@ struct CollapsibleContent<Section, Content>: View where Section: View, Content: 
     var section: () -> Section
     var content: () -> Content
     
+    private let drawingConstants = DrawingConstants()
+    
     init(contentState: Binding<ContentState>, @ViewBuilder section: @escaping () -> Section, @ViewBuilder content: @escaping () -> Content) {
         self._contentState = contentState
         self.section = section
@@ -19,16 +21,60 @@ struct CollapsibleContent<Section, Content>: View where Section: View, Content: 
     }
     
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            sectionView
+            if contentState == .expanded {
+                content()
+                    .transition(.asymmetric(insertion: .scale, removal: .identity)) // FIXME: Fix this transition to scale from top-leading
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private var sectionView: some View {
+        section()
+            .padding(drawingConstants.sectionPadding)
+            .background(sectionBackground)
+            .onTapGesture {
+                print("\(contentState)", terminator: ":")
+                withAnimation(.linear) {
+                    contentState = (contentState == .expanded) ? .collapsed : .expanded
+                }
+                print("\(contentState)")
+            }
+    }
+    
+    private var sectionBackground: some View {
+        RoundedRectangle(cornerRadius: drawingConstants.sectionBackgroundCornerRadius)
+            .foregroundStyle(drawingConstants.sectionBackgroundColor)
+    }
+    
+    private struct DrawingConstants {
+        let sectionPadding: CGFloat = 16
+        let sectionBackgroundCornerRadius: CGFloat = 5
+        let sectionBackgroundColor = Color.gray.opacity(0.3)
+    }
+}
+
+struct CollapsibleContentPreview: View {
+    @State var contentState = ContentState.expanded
+    @State var date = Date()
+    
+    var body: some View {
+        CollapsibleContent(contentState: $contentState) {
+            HStack {
+                Image(systemName: "calendar")
+                Text("Pick a date").font(.caption.bold())
+                Spacer()
+            }
+        } content: {
+            DatePicker("", selection: $date, in: Date()..., displayedComponents: [.date])
+                .datePickerStyle(.graphical)
+        }
     }
 }
 
 #Preview {
-    @State var contentState = ContentState.expanded
-    return CollapsibleContent(contentState: $contentState) {
-        
-    } content: {
-        
-    }
-
+    CollapsibleContentPreview()
+        .preferredColorScheme(.dark)
 }
