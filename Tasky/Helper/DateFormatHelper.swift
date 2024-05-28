20bda12ead63ae2ac37cdf8db3125ed60e437200
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-05-28 22:16:52 +0200

    refactor date formatting into separate file

---
+++ Tasky/Helper/DateFormatHelper.swift
@@ -0,0 +1,46 @@
+//
+//  DateFormatHelper.swift
+//  Tasky
+//
+//  Created by Mohsin Ali Ayub on 28.05.24.
+//
+
+import Foundation
+
+enum DateFormatHelper {
+    static func string(from date: Date) -> String? {
+        let difference = Calendar.current.dateComponents([.year, .month, .day], from: Date(), to: date)
+        
+        if let day = difference.day {
+            let components = Calendar.current.dateComponents([.weekday], from: date)
+            if let weekday = components.weekday, let string = dayOfWeek(weekday) {
+                return "Due \(string)"
+            } else {
+                return "Due \(day) day\(day > 1 ? "s" : "") later"
+            }
+        }
+        
+        return nil
+    }
+    
+    private static func dayOfWeek(_ day: Int) -> String? {
+        switch day {
+        case 1:
+            return "Sunday"
+        case 2:
+            return "Monday"
+        case 3:
+            return "Tuesday"
+        case 4:
+            return "Wednesday"
+        case 5:
+            return "Thursday"
+        case 6:
+            return "Friday"
+        case 7:
+            return "Saturday"
+        default:
+            return nil
+        }
+    }
+}
